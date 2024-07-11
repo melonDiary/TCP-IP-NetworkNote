425df485ba3ac84cbd22d9e4173d3c0be1ddf3df
melonDiary <957071117@qq.com>
2024-07-11 15:38:45 +0000

    fix: 修复代码错误用法

--- ch15/stdcpy.c
+++ ch15/stdcpy.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 #define BUF_SZIE 3
 
 int main(int argc, char *argv[])
@@ -7,8 +8,8 @@ int main(int argc, char *argv[])
     FILE *fp2;
     char buf[BUF_SZIE];
 
-    fp1 = open("news.txt", "r");
-    fp2 = open("cpy.txt", "w");
+    fp1 = fopen("news.txt", "r");
+    fp2 = fopen("cpy.txt", "w");
 
     while (fgets(buf, BUF_SZIE, fp1) != NULL)
         fputs(buf, fp2);
--- ch15/todes.c
+++ ch15/todes.c
@@ -7,7 +7,7 @@ int main()
     int fd = open("data.dat", O_WRONLY | O_CREAT | O_TRUNC);
     if (fd == -1)
     {
-        fputs("file open error");
+        printf("file open error");
         return -1;
     }
 
